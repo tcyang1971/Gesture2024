4db1f202e9f25d1d20e5fef735fc619dba6d38f4
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-05-15 17:51:42 +0800

    v4_拖曳碰撞

--- app/src/main/java/com/example/gesture/MainActivity.kt
+++ app/src/main/java/com/example/gesture/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.gesture
 
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,13 +24,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import com.example.gesture.ui.theme.GestureTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +46,9 @@ class MainActivity : ComponentActivity() {
                     //Greeting("Android")
                     //PointerEvents()
                     Tap()
+                    Drag_Horizontal()
+                    Drag_Vertical()
+                    Drag()
                 }
             }
         }
@@ -117,6 +130,100 @@ fun Tap() {
 
 
         )
+    }
+}
+
+
+@Composable
+fun Drag_Horizontal() {
+    var offsetX by remember { mutableStateOf(0f) }
+    Text(
+        text = "水平拖曳",
+        modifier = Modifier
+            .offset { IntOffset(offsetX.toInt(), 80) }
+            .draggable(
+                orientation= Orientation.Horizontal,
+                state = rememberDraggableState{ delta ->
+                    offsetX += delta
+                }
+            )
+
+    )
+}
+
+@Composable
+fun Drag_Vertical() {
+    var offsetY by remember { mutableStateOf(0f) }
+    Text(
+        text = "垂直拖曳",
+        modifier = Modifier
+            .offset { IntOffset(500, offsetY.toInt()) }
+            .draggable(
+                orientation= Orientation.Vertical,
+                state = rememberDraggableState{ delta ->
+                    offsetY += delta
+                }
+            )
+    )
+}
+
+@Composable
+fun Drag() {
+    var offset1 by remember { mutableStateOf(Offset.Zero) }
+    var offset2 by remember { mutableStateOf(Offset(500f,500f)) }
+    var height by remember { mutableStateOf(0) }
+    var width by remember { mutableStateOf(0) }
+    Box(modifier = Modifier.fillMaxSize()){
+        Box(modifier = Modifier
+            .align(Alignment.TopEnd)
+        ){
+            var r1: Rect = Rect(offset1.x.toInt(), offset1.y.toInt(),
+                offset1.x.toInt()+width, offset1.y.toInt()+height)
+            var r2: Rect = Rect(offset2.x.toInt(), offset2.y.toInt(),
+                offset2.x.toInt()+width, offset2.y.toInt()+height)
+
+            if(r1.intersect(r2)) {
+                Text(text = "碰撞")
+            }
+            else{
+                Text(text = "")
+            }
+        }
+
+
+
+        Box(modifier = Modifier
+            .onGloballyPositioned { coordinates ->
+                height = coordinates.size.height
+                width = coordinates.size.width
+            }
+            .offset { IntOffset(offset1.x.roundToInt(), offset1.y.roundToInt()) }
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    offset1 += dragAmount
+                }
+            }
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ghost1),
+                contentDescription = "精靈1",
+            )
+        }
+
+        Box(modifier = Modifier
+            .offset { IntOffset(offset2.x.roundToInt(), offset2.y.roundToInt()) }
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    offset2 += dragAmount
+                }
+            }
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ghost2),
+                contentDescription = "精靈2",
+            )
+        }
+
     }
 }
 
